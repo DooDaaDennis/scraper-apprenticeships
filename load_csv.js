4584843c02557dfc6f29541fb38b78625deeb6ff
DooDaaDennis <dennis.applebaum@btinternet.com>
2025-05-09 23:16:27 +0100

    inserts csv to database

--- load_csv.js
+++ load_csv.js
@@ -1,26 +1,44 @@
-//connect to mongo shell and load("load_csv.js")
-
-const dbname = db.getSiblingDB("Apprenticeships");
-const final = dbname.getCollection("app-narts-provider-level-fwk-std");
-
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 const fs = require("fs");
 
-const data = fs.readFileSync("app-narts-provider-level-fwk-std.csv", "utf8");
-const rows = data.split("\n");
+async function main() {
+  const client = new MongoClient(process.env.DATABASE);
 
-const fieldnames = rows[0].split(",");
-const results = [];
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
 
-for (let i = 1; i < rows.length; i++) {
-  const values = rows[i].split(",");
-  const documents = {};
+    const dbname = client.db("Apprenticeships");
+    const final = dbname.collection("app-narts-provider-level-fwk-std");
 
-  for (let j = 0; j < fieldnames.length; j++) {
-    documents[fieldnames[j]] = values[j];
+    const data = fs.readFileSync(
+      "app-narts-provider-level-fwk-std.csv",
+      "utf8"
+    );
+    const rows = data.split("\n");
+    const fieldnames = rows[0].split(",");
+    const results = [];
+
+    for (let i = 1; i < rows.length; i++) {
+      const values = rows[i].split(",");
+      const documents = {};
+
+      for (let j = 0; j < fieldnames.length; j++) {
+        documents[fieldnames[j]] = values[j];
+      }
+
+      console.log(`Processing line ${i + 1} of ${rows.length}`);
+      results.push(documents);
+    }
+    console.log("Inserting documents...");
+    await final.insertMany(results);
+    console.log("Documents inserted successfully!");
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    await client.close();
   }
-  console.log(`line ${i + 1} of ${rows.length}`);
-  results.push(documents);
 }
-final.insertMany(results);
 
-console.log("documents inserted successfully!");
+main();
